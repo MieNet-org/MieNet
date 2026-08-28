80f3d38066de38fc5b7914aedfa940625840a276
kiefersv <reronex@gmail.com>
2026-08-28 14:40:45 -0500

    testing ready to go

--- ci_test/test_integration.py
+++ ci_test/test_integration.py
@@ -9,7 +9,8 @@ from mienet import MieNet
 
 def test_ai():
     # ==== Set up
-    ma = MieNet(default_model_location=os.path.dirname(__file__) + '/../docs/tutorial_files/')
+    loc = os.path.dirname(__file__) + '/../docs/tutorial_files/'
+    ma = MieNet(default_model_location=loc)
     test_vars = ['qext', 'qsca', 'asym', 'wavelength']
 
     # ==== Standard Ai run
@@ -20,49 +21,38 @@ def test_ai():
             'Fe': np.linspace(1, 0, 8),
         }
     )
-    print(np.sum(extinction))
-    print(np.sum(scattering))
-    print(np.sum(asymmetry))
     assert np.isclose(np.sum(extinction), 37.19869)
     assert np.isclose(np.sum(scattering), 12.870874)
     assert np.isclose(np.sum(asymmetry), -23.71189)
 
     # ==== Use load grid model
-    ma = MieNet(default_model_location=os.path.dirname(__file__) + '/../docs/tutorial_files/', load_ai_model='TUTORIAL_MODEL')
+    ma = MieNet(default_model_location=loc, load_ai_model='TUTORIAL_MODEL')
     extinction, scattering, asymmetry = ma.ai_efficiencies(
-        np.logspace(-0.5, 1, 8), np.logspace(1.1, 1.9, 8),
+        np.logspace(-0.5, 1, 8), np.logspace(-3, -2, 8),
         {
-            'MgSiO3': np.linspace(0, 1, 8),
+            'Mg2SiO4': np.linspace(0, 1, 8),
             'Fe': np.linspace(1, 0, 8),
-            'TiO2': np.linspace(1, 0, 8)
         }
     )
-    print(np.sum(extinction))
-    print(np.sum(scattering))
-    print(np.sum(asymmetry))
-    assert np.isclose(np.sum(extinction), 135.84726572036743)
-    assert np.isclose(np.sum(scattering), 110.76739168167114)
-    assert np.isclose(np.sum(asymmetry), 41.40172505378723)
+    assert np.isclose(np.sum(extinction), 37.19869)
+    assert np.isclose(np.sum(scattering), 12.870874)
+    assert np.isclose(np.sum(asymmetry), -23.71189)
 
     # ==== Test float input
-    extinction, scattering, asymmetry = ma.ai_efficiencies(3, 1,
+    extinction, scattering, asymmetry = ma.ai_efficiencies(3, 0.005,
         {
-            'MgSiO3': [0.4],
+            'Mg2SiO4': [0.4],
             'Fe': [0.4],
-            'TiO2': [0.4]
         }
     )
-    print(np.sum(extinction))
-    print(np.sum(scattering))
-    print(np.sum(asymmetry))
-    assert np.isclose(np.sum(extinction), 2.857503890991211)
-    assert np.isclose(np.sum(scattering), 2.3773093223571777)
-    assert np.isclose(np.sum(asymmetry), 0.39067474007606506)
+    assert np.isclose(np.sum(extinction), 0.33872768)
+    assert np.isclose(np.sum(scattering), 0.04487792)
+    assert np.isclose(np.sum(asymmetry), -0.3625094)
 
     # ==== Test wrong model load
     testcase = unittest.TestCase()
     with testcase.assertRaises(ValueError):
-        ma = MieNet(default_model_location='files/', load_ai_model='NON_EXISTING')
+        ma = MieNet(default_model_location=loc, load_ai_model='NON_EXISTING')
 
     # ==== Test non-initialisation error
     with testcase.assertRaises(ValueError):
@@ -70,57 +60,57 @@ def test_ai():
         _, _, _ = ma.ai_efficiencies(None, None, None)
 
 
-# def test_grid():
-#     # ==== Set up
-#     ma = MieNet(use_ai=False, mute=False)
-#     test_vars = ['qext', 'qsca', 'asym', 'wavelength']
-#
-#     # ==== create tiny grid
-#     ds = ma.produce_efficiency_grid(
-#         ['SiO2', 'Fe'], wavelengths=np.logspace(-1 ,1.3 ,10),
-#         particle_sizes=np.logspace(1,2 ,40), vmr_data_points=4,
-#         save_file='grid_test.nc'
-#     )
-#     expected_vals = [3373.4240545112407, 2735.199995088339, 1124.5631941617262,
-#                      44.7320528831799]
-#     for t, test in enumerate(test_vars):
-#         assert np.isclose(np.sum(ds[test]), expected_vals[t])
-#
-#     # ==== read in grid
-#     ma.load_grid_efficiency(file_name='grid_test.nc')
-#     lo = ma.default_grids['grid_test.nc']['ds']
-#     for t, test in enumerate(test_vars):
-#         assert np.isclose(np.sum(lo[test]), expected_vals[t])
-#     assert ['SiO2', 'Fe'] == lo.attrs['species']
-#     testcase = unittest.TestCase()
-#     with testcase.assertRaises(ValueError):
-#         ma.load_grid_efficiency(file_name='grid_that_does_not_exist.nc')
-#
-#     # === use grid evaluation
-#     extinction, scattering, asymmetry = ma.grid_efficiencies(
-#         np.logspace(-0.5, 1, 8), np.logspace(1.1, 1.9, 8),
-#         {'SiO2': np.linspace(0, 1, 8), 'Fe': np.linspace(1, 0, 8)}
-#     )
-#     assert np.isclose(np.sum(extinction), 134.77024714311165)
-#     assert np.isclose(np.sum(scattering), 108.05183641939178)
-#     assert np.isclose(np.sum(asymmetry), 45.20338788423523)
-#
-#     # === use grid evaluation with imediate read in
-#     extinction, scattering, asymmetry = ma.grid_efficiencies(
-#         np.logspace(-0.5, 1, 8), np.logspace(1.1, 1.9, 8),
-#         {'SiO2': np.linspace(0, 1, 8), 'Fe': np.linspace(1, 0, 8)},
-#         grid_file='grid_test.nc'
-#     )
-#     assert np.isclose(np.sum(extinction), 134.77024714311165)
-#     assert np.isclose(np.sum(scattering), 108.05183641939178)
-#     assert np.isclose(np.sum(asymmetry), 45.20338788423523)
-#
-#     # ==== request species that are not available
-#     with testcase.assertRaises(ValueError):
-#         extinction, scattering, asymmetry = ma.grid_efficiencies(
-#             np.logspace(-0.5, 1, 8), np.logspace(1.1, 1.9, 8),
-#             {'Not': np.linspace(0, 1, 8), 'Exist': np.linspace(1, 0, 8)},
-#         )
-#
-#     # ==== finish up
-#     os.remove('grid_test.nc')
+def test_grid():
+    # ==== Set up
+    ma = MieNet(use_ai=False, mute=False)
+    test_vars = ['qext', 'qsca', 'asym', 'wavelength']
+
+    # ==== create tiny grid
+    ds = ma.produce_efficiency_grid(
+        ['SiO2', 'Fe'], wavelengths=np.logspace(-1 ,1.3 ,10),
+        particle_sizes=np.logspace(1,2 ,40), vmr_data_points=4,
+        save_file='grid_test.nc'
+    )
+    expected_vals = [3373.4240545112407, 2735.199995088339, 1124.5631941617262,
+                     44.7320528831799]
+    for t, test in enumerate(test_vars):
+        assert np.isclose(np.sum(ds[test]), expected_vals[t])
+
+    # ==== read in grid
+    ma.load_grid_efficiency(file_name='grid_test.nc')
+    lo = ma.default_grids['grid_test.nc']['ds']
+    for t, test in enumerate(test_vars):
+        assert np.isclose(np.sum(lo[test]), expected_vals[t])
+    assert ['SiO2', 'Fe'] == lo.attrs['species']
+    testcase = unittest.TestCase()
+    with testcase.assertRaises(ValueError):
+        ma.load_grid_efficiency(file_name='grid_that_does_not_exist.nc')
+
+    # === use grid evaluation
+    extinction, scattering, asymmetry = ma.grid_efficiencies(
+        np.logspace(-0.5, 1, 8), np.logspace(1.1, 1.9, 8),
+        {'SiO2': np.linspace(0, 1, 8), 'Fe': np.linspace(1, 0, 8)}
+    )
+    assert np.isclose(np.sum(extinction), 134.77024714311165)
+    assert np.isclose(np.sum(scattering), 108.05183641939178)
+    assert np.isclose(np.sum(asymmetry), 45.20338788423523)
+
+    # === use grid evaluation with imediate read in
+    extinction, scattering, asymmetry = ma.grid_efficiencies(
+        np.logspace(-0.5, 1, 8), np.logspace(1.1, 1.9, 8),
+        {'SiO2': np.linspace(0, 1, 8), 'Fe': np.linspace(1, 0, 8)},
+        grid_file='grid_test.nc'
+    )
+    assert np.isclose(np.sum(extinction), 134.77024714311165)
+    assert np.isclose(np.sum(scattering), 108.05183641939178)
+    assert np.isclose(np.sum(asymmetry), 45.20338788423523)
+
+    # ==== request species that are not available
+    with testcase.assertRaises(ValueError):
+        extinction, scattering, asymmetry = ma.grid_efficiencies(
+            np.logspace(-0.5, 1, 8), np.logspace(1.1, 1.9, 8),
+            {'Not': np.linspace(0, 1, 8), 'Exist': np.linspace(1, 0, 8)},
+        )
+
+    # ==== finish up
+    os.remove('grid_test.nc')
