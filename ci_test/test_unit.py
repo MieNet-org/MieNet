ef2b15be3db15557983bf6ab296a89f42b4fa862
d-attaway <daisyattaway@gmail.com>
2026-08-28 15:27:58 -0500

    working on ai model test

--- ci_test/test_unit.py
+++ ci_test/test_unit.py
@@ -17,19 +17,20 @@ def test_sub_functions():
 
 def create_ai_model():
     # ==== Test generate_training_set
-    ma = MieNet(use_ai=True, mute=False, default_model_location='../ci_test/')
+    ma = MieNet(mute=False, default_model_location='../ci_test/')
     ma.generate_training_set('test_set', species=['SiO2', 'MgSiO3'],
                              wavelength_sample=(0.1, 10, 25), particle_size_sample=(0.001, 0.01, 25))
 
     # ==== Test train_ai_model
     dataset = xr.open_dataset('../ci_test/test_set.nc')
     dataset_arr = dataset['data'].to_numpy()
-
-    # set model parameters
-    model_params = {'name': 'tutorial_model',  # file name of AI model
-                    'layers': 2}  # number of hidden layers
-    # optional parameters: nodes, activation_function, optimizer, loss, metrics, batch_size, epochs,
-    # log_wavelength, log_particle_size, log_extinction, log_scattering
-
-    # train your model
     ma.train_ai_model(dataset_arr, model_params={'name': 'test_model', 'layers': 1}, plot_training=False)
+
+    # ==== Test created model predictions
+    ma = MieNet(mute=False, default_model_location='../ci_test/')
+    extinction, scattering, asymmetry = ma.ai_efficiencies(np.linspace(0.1, 10, 8),
+                                                           np.linspace(0.001, 0.01, 8),
+                                                           {'SiO2': np.linspace(0, 1, 8),
+                                                            'MgSiO3': np.linspace(1, 0, 8)})
+
+    assert np.isclose(np.sum(extinction), )
