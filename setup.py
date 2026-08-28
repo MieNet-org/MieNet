016ff72aeec6ce3a7efb29628b9c6ec7971e2c9a
d-attaway <daisyattaway@gmail.com>
2026-08-28 18:54:43 -0500

    add requirements to setup file

--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
 """ set up file """
 from setuptools import setup, find_packages
 
+requirements = ['numpy',
+                'xarray',
+                'miepython',
+                'scipy',
+                'matplotlib',
+                'pyyaml',
+                'h5netcdf']
+
 setup(
     name='mienet',
     version='v0.1',
@@ -9,4 +17,5 @@ setup(
     url='',
     author='Daisy Attaway',
     description='machine learning mie calculations',
+    install_requires=requirements
 )
