d749c3770bafdb823188e9e452afd2a62de162f2
kiefersv <reronex@gmail.com>
2026-08-29 21:56:46 -0500

    move plotting routines to own file

--- mienet/model_handling.py
+++ mienet/model_handling.py
@@ -9,8 +9,9 @@ import shutil
 import yaml
 import numpy as np
 import xarray as xr
-import matplotlib.pyplot as plt
 from datetime import datetime
+
+from .plot_routines import plot_train_ai_model
 
 def get_models(data_location, overwrite=True, url=None):
     """
@@ -414,40 +415,8 @@ def train_ai_model(self, file_name, model_params={}, plot_training=False, overwr
     model.save(self.data_path + model_params['name'] + '.keras')
 
     # ==== PLOT LOSS AND ACCURACY ======================================================================================
-    if plot_training == True:
-
-        # Allow plotting
-        self.mute = False
-
-        epoch = range(1, model_params['epochs'] + 1)
-
-        plt.figure()
-        plt.plot(epoch, history.history['extinction_mae'], label='Extinction', color='blue')
-        plt.plot(epoch, history.history['scattering_mae'], color='green', label='Scattering')
-        plt.plot(epoch, history.history['asymmetry_mae'], color='#57B9FF', label='Asymmetry')
-        plt.plot(epoch, history.history['val_extinction_mae'], label='Val. Extinction', alpha=0.5, color='blue')
-        plt.plot(epoch, history.history['val_scattering_mae'], label='Val. Scattering', color='green', alpha=0.5)
-        plt.plot(epoch, history.history['val_asymmetry_mae'], label='Val. Asymmetry', color='#57B9FF', alpha=0.5)
-        plt.xlabel('Epoch')
-        plt.ylabel('Accuracy')
-        plt.title(f'Model {model_params['name']} Accuracy')
-        plt.legend()
-        plt.tight_layout()
-        plt.show()
-
-        plt.figure()
-        plt.plot(epoch, history.history['extinction_loss'], label='Extinction', color='blue')
-        plt.plot(epoch, history.history['scattering_loss'], color='green', label='Scattering')
-        plt.plot(epoch, history.history['asymmetry_loss'], color='#57B9FF', label='Asymmetry')
-        plt.plot(epoch, history.history['val_extinction_loss'], label='Val. Extinction', alpha=0.5, color='blue')
-        plt.plot(epoch, history.history['val_scattering_loss'], label='Val. Scattering', color='green', alpha=0.5)
-        plt.plot(epoch, history.history['val_asymmetry_loss'], label='Val. Asymmetry', color='#57B9FF', alpha=0.5)
-        plt.xlabel('Epoch')
-        plt.ylabel('Loss')
-        plt.title(f'Model {model_params['name']} Loss')
-        plt.legend()
-        plt.tight_layout()
-        plt.show()
+    if plot_training:
+        plot_train_ai_model(model_params, history)
 
     # ==== ADD MODEL TO CONFIG FILE ====================================================================================
     # path to config file
---
+++ mienet/plot_routines.py
@@ -0,0 +1,44 @@
+""" Plotting functions, this file is not tested"""
+import matplotlib.pyplot as plt
+
+def plot_train_ai_model(model_params, history):
+        """
+        Plot for ai training success.
+
+        Parameters
+        ----------
+        model_params : dict
+            Dictionary of model parameters.
+        history : tensorflow object
+            History of AAN training.
+        """
+
+        epoch = range(1, model_params['epochs'] + 1)
+
+        plt.figure()
+        plt.plot(epoch, history.history['extinction_mae'], label='Extinction', color='blue')
+        plt.plot(epoch, history.history['scattering_mae'], color='green', label='Scattering')
+        plt.plot(epoch, history.history['asymmetry_mae'], color='#57B9FF', label='Asymmetry')
+        plt.plot(epoch, history.history['val_extinction_mae'], label='Val. Extinction', alpha=0.5, color='blue')
+        plt.plot(epoch, history.history['val_scattering_mae'], label='Val. Scattering', color='green', alpha=0.5)
+        plt.plot(epoch, history.history['val_asymmetry_mae'], label='Val. Asymmetry', color='#57B9FF', alpha=0.5)
+        plt.xlabel('Epoch')
+        plt.ylabel('Accuracy')
+        plt.title(f'Model {model_params['name']} Accuracy')
+        plt.legend()
+        plt.tight_layout()
+        plt.show()
+
+        plt.figure()
+        plt.plot(epoch, history.history['extinction_loss'], label='Extinction', color='blue')
+        plt.plot(epoch, history.history['scattering_loss'], color='green', label='Scattering')
+        plt.plot(epoch, history.history['asymmetry_loss'], color='#57B9FF', label='Asymmetry')
+        plt.plot(epoch, history.history['val_extinction_loss'], label='Val. Extinction', alpha=0.5, color='blue')
+        plt.plot(epoch, history.history['val_scattering_loss'], label='Val. Scattering', color='green', alpha=0.5)
+        plt.plot(epoch, history.history['val_asymmetry_loss'], label='Val. Asymmetry', color='#57B9FF', alpha=0.5)
+        plt.xlabel('Epoch')
+        plt.ylabel('Loss')
+        plt.title(f'Model {model_params['name']} Loss')
+        plt.legend()
+        plt.tight_layout()
+        plt.show()
